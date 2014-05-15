5193986a3d4c7481c64ca9319d2da19e172ffcd3
smith.h.neil <smith.h.neil@gmail.com>
2014-05-14 21:32:13 -0500

    utilities tests

---
+++ tests/DotNetdit.UnitTests/Utilities/ExceptioinUtilsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using DotNetdit.Utilities;
+
+namespace DotNetdit.UnitTests.Utilities
+{
+    [TestFixture]
+    public class ExceptioinUtilsTests
+    {
+        [TestCase(null, null)]
+        [TestCase("paramName", null)]
+        [TestCase(null, "message")]
+        public void ThrowIfNull_NullSource_ThrowsException(string paramName, string message) {
+            string nullThing = null;
+
+            Assert.Catch<ArgumentNullException>(() => nullThing.ThrowIfNull(paramName, message));
+        }
+
+        [TestCase(null, null)]
+        [TestCase("paramName", null)]
+        [TestCase(null, "message")]
+        public void ThrowIfNull_NotNullSource_ThrowsException(string paramName, string message) {
+            string notNullThing = "test";
+
+            Assert.DoesNotThrow(() => notNullThing.ThrowIfNull(paramName, message));
+        }
+    }
+}
---
+++ tests/DotNetdit.UnitTests/Utilities/StringUtilsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DotNetdit.Utilities;
+using NUnit.Framework;
+
+namespace DotNetdit.UnitTests.Utilities
+{
+    [TestFixture]
+    public class StringUtilsTests
+    {
+        [Test]
+        public void FormatWith_NullSource_ThrowsException() {
+            string nullString = null;
+
+            Assert.Catch<ArgumentNullException>(() => nullString.FormatWith(""));
+        }
+
+        [Test]
+        public void FormatWith_NullArgs_ThrowsException() {
+            string notNullString = "test {0}";
+
+            Assert.Catch<ArgumentNullException>(() => notNullString.FormatWith(null));
+        }
+
+        [Test]
+        public void FormatWith_ValidInput_ReturnsFormattedString() {
+            string notNullString = "test {0} {1} {2}";
+
+            var result = notNullString.FormatWith(1, 2, 3);
+
+            Assert.That(result, Is.EqualTo("test 1 2 3"));
+        }
+    }
+}
